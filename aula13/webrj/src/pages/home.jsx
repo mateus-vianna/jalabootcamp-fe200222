ac3ff9875c5790f15db3725efce979946e564894
mawavial <panzerkovic@gmail.com>
2022-12-08 16:46:23 -0300

    adds more classes and introduction to react

---
+++ aula13/webrj/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../css/global.css'
+
+
+const Home = () => {
+
+    return (
+        <div className='ml-5 mt-5'>
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home
