1131cac4c61a62a109062dd2d50538fab29f9b60
mawavial <panzerkovic@gmail.com>
2023-01-09 17:14:07 -0300

    adds redux-persist into project

--- aula26/webrj/src/redux/store.jsx
+++ aula26/webrj/src/redux/store.jsx
@@ -1,12 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import counterSlice from './reducers/counterSlice'
 import userReducer from './reducers/userReducer'
-import { counter } from './reducers/counterReducer'
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2'
+import storage from 'redux-persist/lib/storage'
+
+const configStore = {
+    key: 'dogs',
+    stateReconciler: autoMergeLevel2,
+    storage,
+}
+
+const reducer = {
+    counter: counterSlice,
+    users: userReducer
+}
+
+const persitedReducer = persistReducer(configStore, combineReducers(reducer))
+
+
+// export default configureStore({
+//     reducer: { persitedReducer }
+// })
 
 export default configureStore({
-    reducer: {
-        counter: counterSlice,
-        contador: counter,
-        users: userReducer
-    }
+    reducer: persitedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 })
