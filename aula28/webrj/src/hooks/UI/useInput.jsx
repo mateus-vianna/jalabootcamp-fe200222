b98248959b8e9b41a96f52bb0c406a75aeecd79e
mawavial <panzerkovic@gmail.com>
2023-01-12 00:02:14 -0300

    typescript

--- aula28/webrj/src/hooks/UI/useInput.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react'
-
-export function useInput(init) {
-    const [value, setValue] = useState(init)
-    return [
-        { value, onChange: (e) => setValue(e.targe.value) },
-        () => setValue(init)
-    ]
-}
