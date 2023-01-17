0aa944a3a36512bf926dec83ff642d67759ef0b2
Rafael de Paula <xspynks@gmail.com>
2023-01-16 22:47:40 -0300

    feat: add CRUD thunk actions, add error handling messages, add remove and update options on the component, add thunk consts on the userReduce

--- aula27/webrj/src/pages/admin/user/listUsers.jsx
+++ aula27/webrj/src/pages/admin/user/listUsers.jsx
@@ -1,37 +1,61 @@
-import React from 'react'
-import { loadUsersThunk } from '../../../redux/reducers/userReducer'
+import React, { useState } from 'react'
+import { loadUsersThunk, removeUsersThunk, updateUsersThunk } from '../../../redux/reducers/userReducer'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 
-
 const ListUsers = () => {
     const dispatch = useDispatch()
-    const users = useSelector((state) => state.users.users)
+    const { error, status, users, message } = useSelector((state) => state.users)
+    const [updatedUser, setUpdatedUser] = useState(null);
 
     useEffect(() => {
         dispatch(loadUsersThunk())
     }, [])
 
     useEffect(() => {
-        console.log('Users in Users page', users)
-    }, [users])
+        if (updatedUser) {
+            dispatch(updateUsersThunk(updatedUser._id, updatedUser))
+        }
+    }, [updatedUser])
 
+    const handleRemove = async (userId) => {
+        await dispatch(removeUsersThunk(userId))
+    }
 
-    return (<div className='ml-5 mt-5'>
-        <h1>Registered Users</h1>
-        <div className='flex flex-column mt-5 overflow-y container-500'>
-            { users?.map((user, index) => {
-                return (
-                    <div key={ index }>
-                        <p>{ index }</p>
-                        <p>{ user._id }</p>
-                        <p>{ user.name }</p>
-                        <p>{ user.name }</p>
-                    </div>)
-            }) }
+    const handleUpdate = (user) => {
+        setUpdatedUser(user)
+    }
+
+    return (
+    <>
+        <div>
+            { status === 'loading' && <p>{message}</p> }
+            { status === 'failed' && <p>{error}</p> }
+            { status === 'succeeded' && (
+                <div>
+                    { users.length === 0 && <p>No users found</p> }
+                </div>
+            )}
         </div>
-    </div>)
+        <div className='ml-5 mt-5'>
+            <h1>Registered Users</h1>
+            <div className='flex flex-column mt-5 overflow-y container-500'>
+                {users?.map((user) => {
+                    return (
+                        <div key={user._id}>
+                            <p>{user._id}</p>
+                            <p>{user.name}</p>
+                            <button onClick={() => handleRemove(user._id)}>Delete</button>
+                            <button onClick={() => handleUpdate(user)}>Edit</button>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    </>
+    )
+
 }
 
-
 export default ListUsers
+``
--- aula27/webrj/src/services/userService.js
+++ aula27/webrj/src/services/userService.js
@@ -1,13 +1,15 @@
 const url = 'http://localhost:8080/api/users'
-  
-  const createUser = async (body) => {
-        try {
+
+const createUser = async (body) => {
+    try {
         const response = await fetch(url,{
             method: 'post', 
             headers:{
-            'Authorization': `Bearer ${localStorage.getItem('token')}`,
-            'Content-Type': 'application/json'
-        }, body: JSON.stringify(body)});
+                'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
         return await response.json();
     } catch (error) {
         console.log('Error on creating new person from admin', error)
@@ -23,5 +25,34 @@ const getUsers = async () => {
     }
 }
 
-export default createUser;
-export { getUsers };
+const updateUser = async (id, body) => {
+    try {
+        const response = await fetch(`${url}/${id}`, {
+            method: 'put',
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
+        return await response.json();
+    } catch (error) {
+        console.log('Error updating user', error)
+    }
+}
+
+const deleteUser = async (id) => {
+    try {
+        const response = await fetch(`${url}/${id}`, {
+            method: 'delete',
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`,
+            }
+        });
+        return await response.json();
+    } catch (error) {
+        console.log('Error deleting user', error)
+    }
+}
+
+export { createUser, getUsers, updateUser, deleteUser }
