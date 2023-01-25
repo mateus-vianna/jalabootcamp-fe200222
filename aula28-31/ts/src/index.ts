dbc7e3b63f5c0f210f1fef46b2fb2c14953ea8e2
mawavial <panzerkovic@gmail.com>
2023-01-25 15:50:11 -0300

    refact code base

--- aula28-31/ts/src/index.ts
+++ aula28-31/ts/src/index.ts
@@ -1,40 +1,67 @@
-interface IDog {
-    id: number
-    name: string
-    birth: Date
-    age: number
-}
-
-
-const getValue = <T> (source: T, prop: keyof T) => {
-    return source[prop]
-}
-
-const getAllValues = <T> (source: T, func: Function) => {
-    for (const [key, value] of Object.entries(source)) {
-        console.log(`Key: ${key} and Value:  ${value}`)
-    }
-}
-
 interface IContact {
-    contactId: number,
-    contactName: string
+    id: number,
+    name: string
     phone: string
+    role: string
+    auth: boolean
 }
 
 const contact: IContact = {
-    contactId: 1,
-    contactName: 'Name',
-    phone: '12321312321'
+    id: 1,
+    name: 'Name',
+    phone: '12321312321',
+    role: 'DogOwner',
+    auth: true
 }
 
-
-@mult @sum @less
-const oper = (sum: string, multiply: string) => {
-    return
+const user = {
+    id: 1234,
+    roles: ["sysAdmin"],
+    isAuthenticated (): boolean {
+        return true
+    },
+    isInRole (role: string): boolean {
+        return this.roles.contains(role)
+    }
 }
 
-@Injectable
+const authorize = function (target: any, property: string, descriptor: PropertyDescriptor) {
+    const wrapper = descriptor.value
+
+    descriptor.value = function () {
+        if (!user.isAuthenticated()) {
+            throw Error('User not authenticated')
+        }
+
+
+
+
+
+        return wrapper.apply(this, arguments)
+    }
+}
+
 class ContactRepository {
+    private contacts: IContact[] = [];
 
+    @authorize
+    getContactById (id: number): IContact {
+        if (!user.isInRole('sysAdmin'))
+            throw Error('User is not authorized to execute a get contacts')
+        const contact = this.contacts.find(contact => contact.id === id)
+        return contact
+    }
+
+
+    @authorize
+    createNewContact (contact: IContact): void {
+        const exists = this.getContactById(contact.id)
+
+        if (exists) {
+            Object.assign(exists, contact)
+        }
+        else {
+            this.contacts.push(contact)
+        }
+    }
 }
