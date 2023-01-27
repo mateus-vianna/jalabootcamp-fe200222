dfb852212e8c8ce443ae8682198c279338b220f4
mawavial <panzerkovic@gmail.com>
2023-01-27 17:20:27 -0300

    webpack init

---
+++ aula33/ts/dist/main.js
@@ -0,0 +1,1 @@
+document.getElementById("target").innerHTML="Hello World";
---
+++ aula33/ts/src/index.js
@@ -0,0 +1,6 @@
+
+const h1 = document.getElementById('target')
+h1.innerHTML = 'Hello World'
+
+
+//check how to node_modules/webpack
