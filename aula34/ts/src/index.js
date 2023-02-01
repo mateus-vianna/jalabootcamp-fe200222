db807e450f7c8d34ee0be4242b3c0b5a385c5322
mawavial <panzerkovic@gmail.com>
2023-02-01 14:52:38 -0300

    init

---
+++ aula34/ts/src/index.js
@@ -0,0 +1,3 @@
+
+const h1 = document.getElementById('target')
+h1.innerHTML = 'Hello World'
---
+++ aula34/ts/webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, "dist")
+    }
+}
